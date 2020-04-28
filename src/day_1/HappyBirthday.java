b93c63b003a1e1142d894e707c81a3cf7339815e
gulgultekin <gulgutekin1@hotmail.com>
2020-04-28 15:44:08 -0400

    add HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -2,6 +2,6 @@ package day_1;
 
 public class HappyBirthday {
     public static void main(String[] args) {
-        System.out.println("Happy Birthday");
+        System.out.println("Happy Birthday B18");
     }
 }
