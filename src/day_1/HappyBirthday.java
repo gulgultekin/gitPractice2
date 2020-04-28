4432ab9906186f9c711ab970b748ed344b4857ce
gulgultekin <64154918+gulgultekin@users.noreply.github.com>
2020-04-28 15:48:38 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,5 +3,7 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday B18");
+        System.out.println("Happy Birthday");
+        //Edited
     }
 }
